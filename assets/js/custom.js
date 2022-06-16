eb79050791ebdb7c1507097f39eef374ee0ae584
Muhammad Moinuddin <ixmmoinuddin@gmail.com>
2022-06-17 00:31:47 +0500

    responsive UI

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,14 +10,11 @@ $(".slick-carousel").slick({
   slidesToScroll: 1,
   arrows: true,
   dots: true,
-  adaptiveHeight: true,
-  prevArrow:
-    "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-  nextArrow:
-    "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
 });
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
